3163f0be4cefcdc7c0cb537946cbea2b85c418f4
Laurent Montel <montel@kde.org>
2021-12-23 08:04:01 +0100

    GIT_SILENT: it's enabled by default too in qt6

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -33,8 +33,8 @@ int main(int argc, char **argv)
 
 #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling, true);
-#endif
     QGuiApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
+#endif
 #ifdef Q_OS_ANDROID
     QGuiApplication app(argc, argv);
     QQuickStyle::setStyle(QStringLiteral("Material"));
