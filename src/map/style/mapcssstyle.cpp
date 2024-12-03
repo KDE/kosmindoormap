54f60450444781ea0e7d21895e7d5cb8a6969c48
Volker Krause <vkrause@kde.org>
2024-12-03 18:24:14 +0100

    Add infrastructure for determining close way types
    The OSM data model can't distinguish between closed lines and areas on its
    own, so there's commonly a bunch of rules based on other tags applied to
    determine that. We so far have gotten away with that, but the more detailed
    our stylesheets become the more apparent that gets.
    
    This currently only adds three rules (as more rules come at a price
    obviously), but we can express all rules commonly used with this,
    and we can extend this as needed.
    
    Fixes circular highways being rendered as areas and enables fixing wrongly
    outlines water areas.

--- src/map/style/mapcssstyle.cpp
+++ src/map/style/mapcssstyle.cpp
@@ -17,6 +17,49 @@
 
 using namespace KOSMIndoorMap;
 
+// NOTE:
+// - only add rules we really need, this has runtime cost
+// - keep value lists alphabetically sorted
+// - follow rules linked on https://wiki.openstreetmap.org/wiki/Area
+static constexpr inline const char* way_rule_highway_values[] = {
+    "platform",
+    "rest_area",
+    "services"
+};
+static constexpr inline const char* way_rule_natural_values[] = {
+    "bare_rock",
+    "bay",
+    "beach",
+    "crevasse",
+    "desert",
+    "fell",
+    "glacier",
+    "grassland",
+    "heath",
+    "landslide",
+    "mud",
+    "reef",
+    "rock",
+    "sand",
+    "scree",
+    "scrub",
+    "shingle",
+    "sinkhole",
+    "stone",
+    "water",
+    "wetland",
+    "wood"
+};
+
+MapCSSStylePrivate::MapCSSStylePrivate()
+    : m_wayTypeRules({
+        { OSM::TagKey{}, "building", MapCSSObjectType::Area, {}},
+        { OSM::TagKey{}, "highway", MapCSSObjectType::Area, std::span<const char* const>{way_rule_highway_values}},
+        { OSM::TagKey{}, "natural", MapCSSObjectType::Area, std::span<const char* const>{way_rule_natural_values}},
+    })
+{
+}
+
 MapCSSStyle::MapCSSStyle()
     : d(new MapCSSStylePrivate)
 {}
@@ -34,6 +77,12 @@ void MapCSSStyle::compile(OSM::DataSet &dataSet)
 {
     d->m_areaKey = dataSet.tagKey("area");
     d->m_typeKey = dataSet.tagKey("type");
+
+    for (auto &rule : d->m_wayTypeRules) {
+        rule.tag = dataSet.tagKey(rule.tagName);
+    }
+    std::sort(d->m_wayTypeRules.begin(), d->m_wayTypeRules.end(), [](const auto &lhs, const auto &rhs) { return lhs.tag < rhs.tag; });
+
     for (const auto &rule : d->m_rules) {
         rule->compile(dataSet);
     }
@@ -61,8 +110,27 @@ void MapCSSStyle::initializeState(MapCSSState &state) const
             const auto area = state.element.tagValue(d->m_areaKey);
             if (area == "yes") {
                 state.objectType = MapCSSObjectType::Area;
+            } else if (!area.isEmpty()) {
+                state.objectType = MapCSSObjectType::Line;
             } else {
                 state.objectType = MapCSSObjectType::LineOrArea;
+                for (const auto &tag : state.element.way()->tags) {
+                    auto it = std::lower_bound(d->m_wayTypeRules.begin(), d->m_wayTypeRules.end(), tag.key, [](const auto &rule, const auto &key) {
+                        return rule.tag < key;
+                    });
+                    if (it != d->m_wayTypeRules.end() && (*it).tag == tag.key) {
+                        if ((*it).values.empty()) {
+                            state.objectType = (*it).type;
+                        } else {
+                            if (std::binary_search((*it).values.begin(), (*it).values.end(), tag.value)) {
+                                state.objectType = (*it).type;
+                            } else {
+                                state.objectType = (*it).type == MapCSSObjectType::Line ? MapCSSObjectType::Area :MapCSSObjectType::Line;
+                            }
+                        }
+                        break;
+                    }
+                }
             }
             break;
         }
--- src/map/style/mapcssstyle_p.h
+++ src/map/style/mapcssstyle_p.h
@@ -7,12 +7,14 @@
 #ifndef KOSMINDOORMAP_MAPCSSSTYLE_P_H
 #define KOSMINDOORMAP_MAPCSSSTYLE_P_H
 
+#include "mapcssobjecttype_p.h"
 #include "mapcssstyle.h"
 #include "mapcsstypes.h"
 
 #include <osm/element.h>
 
 #include <memory>
+#include <span>
 #include <vector>
 
 namespace KOSMIndoorMap {
@@ -21,12 +23,27 @@ class MapCSSRule;
 
 class MapCSSStylePrivate {
 public:
+    explicit MapCSSStylePrivate();
+
     std::vector<std::unique_ptr<MapCSSRule>> m_rules;
     OSM::StringKeyRegistry<ClassSelectorKey> m_classSelectorRegistry;
     OSM::StringKeyRegistry<LayerSelectorKey> m_layerSelectorRegistry;
 
     OSM::TagKey m_areaKey;
     OSM::TagKey m_typeKey;
+
+    // Rules to determine whether a closed way is a line or area.
+    // see https://wiki.openstreetmap.org/wiki/Area, this is not explicitly represented in the OSM data model
+    // but depends on context.
+    //
+    // For each rule: if tag is present and matches values (empty means the tag just has to be present) we assume type.
+    struct way_type_rule_t {
+        OSM::TagKey tag;
+        const char *tagName;
+        MapCSSObjectType type;
+        std::span<const char* const> values;
+    };
+    std::array<way_type_rule_t, 3> m_wayTypeRules;
 
     inline static MapCSSStylePrivate* get(MapCSSStyle *style) { return style->d.get(); }
 };
